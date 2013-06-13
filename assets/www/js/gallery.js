a9af064e5c37080b07c2c14cf17cf798455dff92
Maricel <eri.mo@sprasia.com.ph>
2013-06-13 09:21:07 +0800

    check entries listed
    check entries listed

--- assets/www/js/gallery.js
+++ assets/www/js/gallery.js
@@ -9,13 +9,15 @@ function viewGallery (){
             fileSys.root.getDirectory("PuzzlePic", {create:true, exclusive: false},
                 function(directory) {
                     alert("inside directory");
+                    alert(directory.fullPath+" -->value returned by getDirectory");
                     //var myPath = fs.root.PuzzlePic;
 					//console.log("Root = " + fs.root.fullPath);
 					var directoryReader = directory.createReader();
 						directoryReader.readEntries(function(entries) {
 							var i;
 							for (i=0; i<entries.length; i++) {
-								console.log(entries[i].name);
+								//console.log(entries[i].name);
+								alert(entries[i]+" entries displayed in " +i);
 							}
 						});
                               
