6cbaddd0a5081cd28370f2abcda891f325f0bc9f
Maricel <eri.mo@sprasia.com.ph>
2013-06-13 08:50:36 +0800

    view gallery
    view gallery

--- assets/www/js/gallery.js
+++ assets/www/js/gallery.js
@@ -1,29 +1,30 @@
 
 
-
-
 function viewGallery (){
 
 	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, 
-	function(fs) {
-				var myPath = fs.root.PuzzlePic;
-				//console.log("Root = " + fs.root.fullPath);
-				var directoryReader = myPath.createReader();
-				directoryReader.readEntries(function(entries) {
-					var i;
-					for (i=0; i<entries.length; i++) {
-						console.log(entries[i].name);
-					}
-				}, function (error) {
-
-						alert(error.code);
-				})
-				//alert(fs);
-				//var entry = fs.root.fullPath;
-				//var myFs = entry.toURI();
-				//alert(entry+" -->myFs");
-
-			}, function (error) {
-				alert(error.code);
-			});
+	
+		function(fileSys) {
+            alert("inside fileSys function");
+            fileSys.root.getDirectory("PuzzlePic", {create:true, exclusive: false},
+                function(directory) {
+                    alert("inside directory");
+                    //var myPath = fs.root.PuzzlePic;
+					//console.log("Root = " + fs.root.fullPath);
+					var directoryReader = directory.createReader();
+						directoryReader.readEntries(function(entries) {
+							var i;
+							for (i=0; i<entries.length; i++) {
+								console.log(entries[i].name);
+							}
+						});
+                              
+                }, resOnError);
+       }, resOnError);
+			
 }
+
+function resOnError(error) { 
+         //console.log("Error creating directory "+error.code); 
+         alert("Error can not create directory" +error.code)
+} 
